d3e3223381167900a98fceef1e9adaf3f6d6dbfb
bendhiabaha5-pixel <bendhiabaha5@gmail.com>
2025-11-24 13:11:08 +0100

    Delete SmartStudy directory

--- SmartStudy/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "controllers/FrontController.php";
-
-$front = new FrontController();
-$front->handleRequest();
--- SmartStudy/views/admin/categories.php
+++
@@ -1,1 +0,0 @@
-<?php include "views/back/html/categories.html"; ?>
